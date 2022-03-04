4a9adafbfd7b27f8f96cf23f81ff41f7d62d070b
carvalhodaniel <carvalhodanielg@gmail.com>
2022-03-04 15:30:06 -0300

    Ativação do submmit button através do 'Enter', no teclado

--- scripts.js
+++ scripts.js
@@ -26,3 +26,35 @@ const emailfield = document.getElementById('emailfield')
 
 })
 
+document.addEventListener("keypress", function(e){
+
+const email = document.getElementById('email')
+const emailfield = document.getElementById('emailfield')
+
+if(e.key === "Enter"){
+    if (email.value == ""){
+        emailfield.classList.add('emailfail')
+        email.value = ""
+        email.placeholder = "Email inválido"
+    }else if (email.value == email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1 || (email.value.indexOf(".") - email.value.indexOf("@") == 1 )){
+        emailfield.classList.add('emailfail')
+        email.value = ""
+        email.placeholder = "Email inválido"
+    } else{emailfield.classList.remove('emailfail')
+    const emailAdress = email.value;
+    email.value = ""
+    //alert("Email cadastrado com sucesso!");
+    Swal.fire({
+        title: "Olá!",
+        text: "Seu email: " + emailAdress + " foi cadastrado com sucesso!",
+        icon: "success",    
+    });
+    }
+    
+}
+
+
+
+})
+
+
